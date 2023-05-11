55314ab537dd7762b11564edcd674fa9ee0d5631
Michael W Jarrett <mwilliamkjarrett@gmail.com>
2023-05-11 19:42:45 +0000

    working on lgf

--- index.js
+++ index.js
@@ -16,16 +16,53 @@ var _ = require('underbar');
  *
  * 4. To test your work, run the following command in your terminal:
  *
- *    npm start --prefix ./<YOUR_GITHUB_FOLDER>
+ *    npm start --prefix ./lgf-test
  *
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount;
+var maleCount = function(array){
+    let count = 0;
+    for(let el of array){
+        if(el.gender === 'male'){
+         count++;
+        }
+    }
+    return count;
+}
 
-var femaleCount;
+var femaleCount = function(arr){
 
-var oldestCustomer;
+    let females = _.reduce(arr, function(acc, curr){
+        
+        if(curr.gender === 'female'){
+            acc++
+        }
+        //always make sure your are returning our accumulator
+        return acc;
+    }, 0)
+
+    return females;
+}
+
+var oldestCustomer = function(arr){
+//leave seed empty
+   let oldest = _.reduce(arr, function(acc, el){
+//acc starts out as first element in array, compare first element to  next element
+      if(el.age > acc.age){ //if age value is greater than acc age value
+       return el; //return element, becomes new acc
+      } else { //pass accumultor to call back agian
+         return acc;
+      }
+
+   })
+   console.log(oldest, 'oldest')
+  return oldest.name
+}
+//find the oldest customers name.
+//reduce, returns an object
+//if the current elements age is greater than seed objects age, replace seed object with age of current element
+//
 
 var youngestCustomer;
 
