ffdd80ec95758bdf868d82b952951da64d76f277
Michael W Jarrett <mwilliamkjarrett@gmail.com>
2023-05-11 20:41:38 +0000

    working on lgf

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
-var customers = require('./data/customers.json');
-var _ = require('underbar');
+var customers = require("./data/customers.json");
+var _ = require("underbar");
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -21,52 +21,78 @@ var _ = require('underbar');
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount = function(array){
-    let count = 0;
-    for(let el of array){
-        if(el.gender === 'male'){
-         count++;
-        }
+var maleCount = function (array) {
+  let count = 0;
+  for (let el of array) {
+    if (el.gender === "male") {
+      count++;
     }
-    return count;
-}
+  }
+  return count;
+};
 
-var femaleCount = function(arr){
-
-    let females = _.reduce(arr, function(acc, curr){
-        
-        if(curr.gender === 'female'){
-            acc++
-        }
-        //always make sure your are returning our accumulator
-        return acc;
-    }, 0)
-
-    return females;
-}
-
-var oldestCustomer = function(arr){
-//leave seed empty
-   let oldest = _.reduce(arr, function(acc, el){
-//acc starts out as first element in array, compare first element to  next element
-      if(el.age > acc.age){ //if age value is greater than acc age value
-       return el; //return element, becomes new acc
-      } else { //pass accumultor to call back agian
-         return acc;
+var femaleCount = function (arr) {
+  let females = _.reduce(
+    arr,
+    function (acc, curr) {
+      if (curr.gender === "female") {
+        acc++;
       }
+      //always make sure your are returning our accumulator
+      return acc;
+    },
+    0
+  );
 
-   })
-   console.log(oldest, 'oldest')
-  return oldest.name
-}
+  return females;
+};
 //find the oldest customers name.
 //reduce, returns an object
 //if the current elements age is greater than seed objects age, replace seed object with age of current element
-//
+var oldestCustomer = (arr) => {
+  //leave seed empty
+  let oldest = _.reduce(arr, function (acc, el) {
+    //acc starts out as first element in array, compare first element to  next element
+    if (el.age > acc.age) {
+      //if age value is greater than acc age value
+      return el; //return element, becomes new acc
+    } else {
+      //pass accumultor to call back agian
+      return acc;
+    }
+  });
+//   console.log(oldest, "oldest");
+  return oldest.name;
+};
+//same as above
+//use reduce to compare elements in the array, returning the fitting our condition
+var youngestCustomer = (arr) => {
+  let youngest = _.reduce(arr, (acc, el) => {
+    //if the current element age is less than acc age
+    if (el.age < acc.age) {
+      //if we find a younger age, return that el
+      return el;
+    } else {//return acc
+      return acc;
+    }
+  });
+  //return result of reduce
+  return youngest.name;
+};
+//need to convert average balance to usable string
+//use reduce
+//set seed to 0
+//convert balance to usable string and add to acc
 
-var youngestCustomer;
+var averageBalance = (arr) => {
 
-var averageBalance;
+    let averageB = _.reduce(arr, (acc, el) => {
+         
+         return acc +=  Number(el.balance.replace(/^(-)|[^0-9.]+/g, '$1'))
+    })
+    console.log(averageB.toFixed(2), 'average')
+   return Number(averageB.toFixed(2))
+}
 
 var firstLetterCount;
 
